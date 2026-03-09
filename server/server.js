78ae6d03446bb6c5411e4955c2de4442d1751b7f
tanvimodI3 <tanvimodi1307@gmail.com>
2026-03-09 23:18:49 +0530

    .

--- server/server.js
+++ server/server.js
@@ -82,7 +82,7 @@ app.post("/s/messages", async(req,res)=>{
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server running yay");
 });
 
